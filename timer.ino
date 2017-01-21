fc20790a8dbf38252067fb192a7c56417f310b33
DongKwangLee <ldk12345@nate.com>
2017-01-21 20:27:12 +0900

    Arduino(4강)_timer
    /*
     *      This program is free software; you can redistribute it and/or modify
     *      it under the terms of the GNU General Public License as published by
     *      the Free Software Foundation; either version 2 of the License, or
     *      (at your option) any later version.
     *
     *      This program is distributed in the hope that it will be useful,
     *      but WITHOUT ANY WARRANTY; without even the implied warranty of
     *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     *      GNU General Public License for more details.
     *
     *      You should have received a copy of the GNU General Public License
     *      along with this program; if not, write to the Free Software
     *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
     *      MA 02110-1301, USA.
     */
    
    /*  * * * * * * * * * * * * * * * * * * * * * * * * * * *
     Code by Simon Monk
     http://www.simonmonk.org
    * * * * * * * * * * * * * * * * * * * * * * * * * * * * */

---
+++ timer.ino
@@ -0,0 +1,35 @@
+#include "Timer.h"                     
+
+const int LED1 = 8;                    
+const int LED2 = 9;                    
+const int LED3 = 10;                    
+const int LED4 = 11;                    
+const unsigned long PERIOD1 = 50;
+const unsigned long PERIOD2 = 60;
+const unsigned long PERIOD3 = 70;
+const unsigned long PERIOD4 = 80;
+
+Timer t;
+
+void setup(void)
+{
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+    pinMode(LED4, OUTPUT);
+    
+    t.oscillate(LED1, PERIOD1, HIGH);
+    t.oscillate(LED2, PERIOD2, HIGH);
+    t.oscillate(LED3, PERIOD3, HIGH);
+    t.oscillate(LED4, PERIOD4, HIGH);
+}
+
+void loop(void)
+{
+    
+    while(1)
+    {
+        t.update();
+    }
+}
+
